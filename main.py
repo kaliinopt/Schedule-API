82d8f4dc88f6d37af2f252089097e773e8276839
kaliinopt <youngtrillpill@gmail.com>
2025-05-12 15:59:46 +0300

    Добавил политику CORS и упростил debug setup

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routers import schedule, user, auth
 from app.logging_conf.loger import setup_logging
 from app.logging_conf.loging_middleware import log_requests
 from app.core.config import load_config
 import logfire
 
+config = load_config()
+
 logfire.configure()
 
-app = FastAPI()
+app = FastAPI(debug=config.DEBUG)
 
 logfire.instrument_fastapi(app)
 
 config = load_config()
 
-if config.DEBUG:
-    app.debug = True
-else:
-    app.debug = False
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "https://frontend.ru",
+    ],
+    allow_methods=["GET", "POST", "PUT", "DELETE"], 
+    allow_headers=["Authorization", "Content-Type"],
+    allow_credentials=True,
+    max_age=86400,  # Кешировать CORS-правила на 24 часа
+)
+
 
 setup_logging()
 
